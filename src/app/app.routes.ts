2b353e2b1448ce45135278147c8ef3f03daf8156
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-02-24 15:16:41 -0300

    route app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './LoginAll/login/login.component';
 import { ResetPasswordComponent } from './LoginAll/reset-password/reset-password.component';
+import { VerificacaouserComponent } from './verificacaouser/verificacaouser.component';
 
 export const routes: Routes = [
   { path: '', component: HeaderComponent },
   { path: 'livros', component: HeaderComponent },
   { path: 'login', component: LoginComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'verify', component: VerificacaouserComponent },
 ];
 
 @NgModule({
