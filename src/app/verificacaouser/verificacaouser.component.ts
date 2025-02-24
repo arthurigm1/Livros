73b617b97d7606a907167fccfab3d2bc54aabf60
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-02-24 18:27:34 -0300

    verificacaouser

--- src/app/verificacaouser/verificacaouser.component.ts
+++ src/app/verificacaouser/verificacaouser.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
@@ -10,52 +10,56 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './verificacaouser.component.html',
   styleUrl: './verificacaouser.component.scss',
 })
-export class VerificacaouserComponent {
+export class VerificacaouserComponent implements OnInit {
+  token: string = '';
   verificado: boolean | null = null;
 
   constructor(
     private route: ActivatedRoute,
-    private http: HttpClient,
     private router: Router,
+    private http: HttpClient,
     private toastService: ToastrService
   ) {}
-  ngOnInit(): void {
-    const code = this.route.snapshot.queryParamMap.get('code');
 
-    if (code) {
-      this.http
-        .get('https://fullstacklivros-production.up.railway.app/verify', {
-          params: { code },
-          responseType: 'text',
-        })
-        .subscribe(
-          (response) => {
-            if (response === 'verify_success') {
-              this.verificado = true;
-              this.toastService.success(
-                'Conta verificada com sucesso!',
-                'Sucesso'
-              );
-              setTimeout(() => this.router.navigate(['/']), 2000);
-            } else {
-              this.verificado = false;
-              this.toastService.error(
-                'Código de verificação inválido.',
-                'Erro'
-              );
-            }
-          },
-          () => {
-            this.verificado = false;
-            this.toastService.error('Erro ao verificar a conta.', 'Erro');
-          }
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.token = params['token'];
+      if (this.token) {
+        this.verifyUser();
+      } else {
+        this.verificado = false;
+        this.toastService.warning(
+          'Nenhum token de verificação encontrado!',
+          'Aviso'
         );
-    } else {
-      this.verificado = false;
-      this.toastService.warning(
-        'Nenhum código de verificação encontrado.',
-        'Atenção'
-      );
-    }
+      }
+    });
+  }
+
+  verifyUser(): void {
+    this.http
+      .get('https://fullstacklivros-production.up.railway.app/verify', {
+        params: { token: this.token },
+        responseType: 'text',
+      })
+      .subscribe({
+        next: (response) => {
+          if (response === 'verify_success') {
+            this.verificado = true;
+            this.toastService.success(
+              'Conta verificada com sucesso!',
+              'Sucesso'
+            );
+            setTimeout(() => this.router.navigate(['/login']), 2000);
+          } else {
+            this.verificado = false;
+            this.toastService.error('Token de verificação inválido!', 'Erro');
+          }
+        },
+        error: () => {
+          this.verificado = false;
+          this.toastService.error('Erro ao verificar a conta!', 'Erro');
+        },
+      });
   }
 }
